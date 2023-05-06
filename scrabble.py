f2e0f790f6637da0eb8a72984c7f58fda9541ee0
Lucas Tymen <lucas.tymen@gmail.com>
2023-05-06 10:36:01 +0200

    step2

--- scrabble.py
+++ scrabble.py
@@ -3,4 +3,6 @@ points = [1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 4, 1, 3, 10, 1, 1, 1, 1, 4, 4, 
 
 letter_to_points = {letters:points for letters, points in zip(letters, points)}
 
+letter_to_points[" "] = 0
+
 print(letter_to_points)
